3b1b7af8b7a7789ca65ac07f6329431a076c2d37
dmlisa200 <17917202+dmlisa200@users.noreply.github.com>
2020-06-23 21:26:43 -0500

    Add files via upload

---
+++ assign_average.py
@@ -0,0 +1,17 @@
+
+
+def switch_average(key):
+    pass
+    """
+    switcher = {
+        'A': 'Ankeny',
+        'B': 'Ames',
+        'C': 'Waukee',
+        'D': 'Grimes',
+        'E': 'Des Moines',
+    }
+    print(switcher.get('C', "Invalid town name"))
+
+if __name__ == '__main__':
+    switch_average('C')
+ """
